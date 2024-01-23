d84de360f803b30557d0a24ab5df50c7233d57d4
superremy <hendriceremy@gmail.com>
2024-01-23 20:45:42 +0100

    Refactor httpsserver

--- HttpsServer/src/main/java/net/HttpsServer.java
+++ HttpsServer/src/main/java/net/HttpsServer.java
@@ -6,42 +6,16 @@ import java.net.Socket;
 import java.security.KeyStore;
 import java.util.stream.Collectors;
 
-public class HttpsServer {
-    private final int port;
-    private final SSLServerSocket serverSocket;
+public class HttpsServer extends SSLServer {
     private final HttpRequestHandler httpHandler;
 
     public HttpsServer(int port, SSLContext sslContext, HttpRequestHandler handler) throws Exception{
-        this.port = port;
-        this.serverSocket = initSocket(sslContext);
+        super(port, sslContext, "HttpsServer");
         this.httpHandler = handler;
     }
 
-    private SSLServerSocket initSocket(SSLContext sslContext) throws Exception{
-        try{
-            SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
-            return (SSLServerSocket) sslServerSocketFactory.createServerSocket(this.port);
-        }catch(Exception ex){
-            throw new Exception(ex);
-        }
-    }
-
-    public void start(){
-        System.out.printf("HttpsServer:: HTTPS server running at 127.0.0.1:%d \n",this.port);
-        //printServerSocketInfo( this.serverSocket);
-        while(true){
-            try{
-                SSLSocket socket = (SSLSocket) this.serverSocket.accept();
-                //socket.setTcpNoDelay(true);
-                //printSocketInfo(socket);
-                new Thread(() -> handleConnection(socket)).start();
-            }catch(IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    private void handleConnection(Socket socket){
+    @Override
+    protected void handleClient(SSLSocket socket){
         try{
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             String clientRequest = read(socket.getInputStream());
@@ -60,38 +34,4 @@ public class HttpsServer {
         } while (inputStream.available() > 0);
         return result.toString();
     }
-
-
-
-    private static void printServerSocketInfo(SSLServerSocket s) {
-        System.out.println("Server socket class: "+s.getClass());
-        System.out.println("   Socket address = "
-                +s.getInetAddress().toString());
-        System.out.println("   Socket port = "
-                +s.getLocalPort());
-        System.out.println("   Need client authentication = "
-                +s.getNeedClientAuth());
-        System.out.println("   Want client authentication = "
-                +s.getWantClientAuth());
-        System.out.println("   Use client mode = "
-                +s.getUseClientMode());
-    }
-
-    private static void printSocketInfo(SSLSocket s) {
-        System.out.println("Socket class: "+s.getClass());
-        System.out.println("   Remote address = "
-                +s.getInetAddress().toString());
-        System.out.println("   Remote port = "+s.getPort());
-        System.out.println("   Local socket address = "
-                +s.getLocalSocketAddress().toString());
-        System.out.println("   Local address = "
-                +s.getLocalAddress().toString());
-        System.out.println("   Local port = "+s.getLocalPort());
-        System.out.println("   Need client authentication = "
-                +s.getNeedClientAuth());
-        SSLSession ss = s.getSession();
-        System.out.println("   Cipher suite = "+ss.getCipherSuite());
-        System.out.println("   Protocol = "+ss.getProtocol());
-    }
-
 }
---
+++ HttpsServer/src/main/java/net/SSLServer.java
@@ -0,0 +1,53 @@
+package net;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import java.io.IOException;
+
+public abstract class SSLServer {
+    private final String serverName;
+    protected boolean keepAlive = true;
+    private final int port;
+    private final SSLServerSocket serverSocket;
+    public SSLServer(int port, SSLContext sslContext, String serverName) throws Exception{
+        this.port = port;
+        this.serverSocket = initSocket(sslContext);
+        this.serverName = serverName;
+    }
+
+    private SSLServerSocket initSocket(SSLContext sslContext) throws Exception{
+        try{
+            SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
+            return (SSLServerSocket) sslServerSocketFactory.createServerSocket(this.port);
+        }catch(Exception ex){
+            throw new Exception(ex);
+        }
+    }
+
+    public final void start(){
+        log(String.format("%s running at 127.0.0.1:%d \n",this.serverName,this.port));
+        while(keepAlive){
+            try{
+                SSLSocket socket = (SSLSocket) this.serverSocket.accept();
+                new Thread(() -> handleClient(socket)).start();
+            }catch(IOException ex) {
+                System.out.println(ex.toString());
+            }
+        }
+    }
+
+    /**
+     * Override this method to handle client connections.
+     * @param sslSocket
+     */
+    protected void handleClient(SSLSocket sslSocket){
+        // Do nothing
+    }
+
+    protected final void log(String message){
+        System.out.println(serverName + ":: " + message);
+    }
+
+}
